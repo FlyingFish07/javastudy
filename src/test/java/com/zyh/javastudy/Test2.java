1133d3e95b4dece1dccaf2d0e78039d154aec89c
FlyingFish07 <yhzhang07@gmail.com>
2016-11-29 23:51:33 +0800

    add test2

---
+++ src/test/java/com/zyh/javastudy/Test2.java
@@ -0,0 +1,12 @@
+package com.zyh.javastudy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Test2 {
+	@Test
+	public void test1(){
+		System.out.println("hello world.");
+		Assert.assertEquals("123", "12");
+	}
+}
